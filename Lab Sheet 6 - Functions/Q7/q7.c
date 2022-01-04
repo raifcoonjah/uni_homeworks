75edd79ed2ae66eab347e247c4be8b8ef0cfe631
Raif Coonjah <raifcoonjah@pm.me>
2022-01-04 23:02:43 +0400

    remove useless files

--- Lab Sheet 6 - Functions/Q7/q7.c
+++ Lab Sheet 6 - Functions/Q7/q7.c
@@ -12,38 +12,10 @@ Mark Letter Grade
 40 ≤ x < 45 D
 30 ≤ x < 40 E
 x < 30 F
-Write a function that accepts an exam score as input and uses a decision structure to find
+Write a function that accepts an exam Your final grade is:  as input and uses a decision structure to find
 the corresponding grade. Display suitable messages if the marks entered do not lie in the
 range 0 – 100.
 */
 
-#include<stdio.h>
-void main()
-{
 
-
-int marks;
-char username[20];
- 
-
-
-
-// I know this is useless but it makes the final result look cooler xD
-
-printf("Please enter your username: ");
-scanf("%s", &username);
-
-printf("Please enter your marks: ");
-scanf("%d", &marks);
-
-if (marks <=0 || marks >= 100){
- printf("Invalid number entered, please try again!");
-
-}
-
-else if (marks >= 90){
- printf("Congratulation, %d you scored an A+", username)
-}
-
-}
-	
+// Can you please explain this one in the next class? Thanks :D
